a3807f947358f43ed7c1db92def97c44bd79a448
Bassel-kh <bassel.kh.svu@gmail.com>
2020-06-12 00:20:19 -0700

    Add two more buttons

--- index.php
+++ index.php
@@ -24,4 +24,7 @@
         echo "<h1>My age is : $age</h1>";
    ?>
 <button type="button" class="btn btn-outline-info">Click Me</button>
+<button type="button" class="btn btn-outline-primary">Primary</button>
+<button type="button" class="btn btn-outline-success">Success</button>
+
 <?php require 'includes/footer.php'?>
